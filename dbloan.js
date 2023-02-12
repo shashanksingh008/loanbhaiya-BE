3b10bbace160307f317926e29bb8817a52f26c4e
-shashanksingh008 <-shashank.singh1096@gmail.com>
2023-02-12 14:18:58 +0530

    added modals and db schema

--- dbloan.js
+++ dbloan.js
@@ -17,7 +17,122 @@ let Loan = [
     _id : ObjectId(),
     name: "Car Loan",
     details : "Home loans @8.45%* p.a. onwards & EMIs of ₹766* per lakh. Credit Score Linked Int. Rates. Check your eligibility and apply online. Get instant approval. Become a homeowner today! Limited Period Offer. Affordable home loans. Lower Interest Rate.",
+    icon : "/car.png",
+    type : "loan",
+    banks : [],
+    agents : [],
+    isDeleted : false,
+    isActive : true
+  },
+  {
+    _id : ObjectId(),
+    name: "Education Loan",
+    details : "Home loans @8.45%* p.a. onwards & EMIs of ₹766* per lakh. Credit Score Linked Int. Rates. Check your eligibility and apply online. Get instant approval. Become a homeowner today! Limited Period Offer. Affordable home loans. Lower Interest Rate.",
+    icon : "/education.png",
+    type : "loan",
+    banks : [],
+    agents : [],
+    isDeleted : false,
+    isActive : true
+  },
+  {
+    _id : ObjectId(),
+    name: "Personal Loan",
+    details : "Home loans @8.45%* p.a. onwards & EMIs of ₹766* per lakh. Credit Score Linked Int. Rates. Check your eligibility and apply online. Get instant approval. Become a homeowner today! Limited Period Offer. Affordable home loans. Lower Interest Rate.",
+    icon : "/personal.png",
+    type : "loan",
+    banks : [],
+    agents : [],
+    isDeleted : false,
+    isActive : true
+  },
+  {
+    _id : ObjectId(),
+    name: "Business Loan",
+    details : "Home loans @8.45%* p.a. onwards & EMIs of ₹766* per lakh. Credit Score Linked Int. Rates. Check your eligibility and apply online. Get instant approval. Become a homeowner today! Limited Period Offer. Affordable home loans. Lower Interest Rate.",
+    icon : "/business.png",
+    type : "loan",
+    banks : [],
+    agents : [],
+    isDeleted : false,
+    isActive : true
+  },
+  {
+    _id : ObjectId(),
+    name: "Gold Loan",
+    details : "Home loans @8.45%* p.a. onwards & EMIs of ₹766* per lakh. Credit Score Linked Int. Rates. Check your eligibility and apply online. Get instant approval. Become a homeowner today! Limited Period Offer. Affordable home loans. Lower Interest Rate.",
+    icon : "/gold.png",
+    type : "loan",
+    banks : [],
+    agents : [],
+    isDeleted : false,
+    isActive : true
+  }
+
+]
+
+
+let CreditCard = [
+
+  {
+    _id : ObjectId(),
+    name: "SBI",
+    details : "Credit Card - SBI credit card offers amazing deals, rewards & offers for all types of spends. Know more about our credit card services. Apply now!",
     icon : "/home.png",
+    type : "creditcard",
+    categories : [{}],
+    isDeleted : false,
+    isActive : true
+  },
+  {
+    _id : ObjectId(),
+    name: "Car Loan",
+    details : "Home loans @8.45%* p.a. onwards & EMIs of ₹766* per lakh. Credit Score Linked Int. Rates. Check your eligibility and apply online. Get instant approval. Become a homeowner today! Limited Period Offer. Affordable home loans. Lower Interest Rate.",
+    icon : "/car.png",
+    type : "loan",
+    banks : [],
+    agents : [],
+    isDeleted : false,
+    isActive : true
+  },
+  {
+    _id : ObjectId(),
+    name: "Education Loan",
+    details : "Home loans @8.45%* p.a. onwards & EMIs of ₹766* per lakh. Credit Score Linked Int. Rates. Check your eligibility and apply online. Get instant approval. Become a homeowner today! Limited Period Offer. Affordable home loans. Lower Interest Rate.",
+    icon : "/education.png",
+    type : "loan",
+    banks : [],
+    agents : [],
+    isDeleted : false,
+    isActive : true
+  },
+  {
+    _id : ObjectId(),
+    name: "Personal Loan",
+    details : "Home loans @8.45%* p.a. onwards & EMIs of ₹766* per lakh. Credit Score Linked Int. Rates. Check your eligibility and apply online. Get instant approval. Become a homeowner today! Limited Period Offer. Affordable home loans. Lower Interest Rate.",
+    icon : "/personal.png",
+    type : "loan",
+    banks : [],
+    agents : [],
+    isDeleted : false,
+    isActive : true
+  },
+  {
+    _id : ObjectId(),
+    name: "Business Loan",
+    details : "Home loans @8.45%* p.a. onwards & EMIs of ₹766* per lakh. Credit Score Linked Int. Rates. Check your eligibility and apply online. Get instant approval. Become a homeowner today! Limited Period Offer. Affordable home loans. Lower Interest Rate.",
+    icon : "/business.png",
+    type : "loan",
+    banks : [],
+    agents : [],
+    isDeleted : false,
+    isActive : true
+  },
+  {
+    _id : ObjectId(),
+    name: "Gold Loan",
+    details : "Home loans @8.45%* p.a. onwards & EMIs of ₹766* per lakh. Credit Score Linked Int. Rates. Check your eligibility and apply online. Get instant approval. Become a homeowner today! Limited Period Offer. Affordable home loans. Lower Interest Rate.",
+    icon : "/gold.png",
     type : "loan",
     banks : [],
     agents : [],
